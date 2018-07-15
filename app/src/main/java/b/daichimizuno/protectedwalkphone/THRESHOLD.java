db885fa97d483ab4bd925d609dc56ef4707c1b13
eiji <80supra.toyota@gmail.com>
2018-07-16 04:46:58 +0900

    センサ値変更

--- app/src/main/java/b/daichimizuno/protectedwalkphone/THRESHOLD.java
+++ app/src/main/java/b/daichimizuno/protectedwalkphone/THRESHOLD.java
@@ -1,7 +1,7 @@
 package b.daichimizuno.protectedwalkphone;
 
 public class THRESHOLD {
-    public static final float ARROUND_ELECTORICS = 80f;
+    public static final float ARROUND_ELECTORICS = 150f;
     public static final float GYRO_PLUS = 0.1f;
     public static final float GYRO_MINUX = -0.1f;
     public static final float ACCELERATION = 0;
